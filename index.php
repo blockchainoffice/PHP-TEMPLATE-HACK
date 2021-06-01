724ba73fcc265dc2f470e4b91c7510b9871c46ab
Blockchain Office <78378074+blockchainoffice@users.noreply.github.com>
2021-06-01 20:22:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
   $FILE = 'html.html';
 
   $REPLACE = [
-   '{#TITLE#}' 		         => 'Simple HTML/PHP - TEMPLATE/PARSER - PHP EVAL HACKS'
-  ,'{#CONTENT#}' 		       => 'Simple HTML/PHP TEMPLATE'
-  ,'{#TIMESTAMP#}' 		     => '<?php echo date("Y"); ?>'
+   '{#TITLE#}'      => 'Simple HTML/PHP - TEMPLATE/PARSER - PHP EVAL HACKS'
+  ,'{#CONTENT#}'    => 'Simple HTML/PHP TEMPLATE'
+  ,'{#TIMESTAMP#}'  => '<?php echo date("Y"); ?>'
   ];
 
   $EVALBOX = (preg_replace(array_keys($REPLACE), $REPLACE, file_get_contents($FILE) ));
